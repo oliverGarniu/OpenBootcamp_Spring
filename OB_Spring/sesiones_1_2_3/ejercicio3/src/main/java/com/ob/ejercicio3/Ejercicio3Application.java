1db41dee49130f920ae830ef2818d069b27c04dd
oliverGarniu <oliver.garniu@gmail.com>
2022-11-11 10:56:43 +0100

    añadir ejercicio 3

--- OB_Spring/sesiones_1_2_3/ejercicio3/src/main/java/com/ob/ejercicio3/Ejercicio3Application.java
+++ OB_Spring/sesiones_1_2_3/ejercicio3/src/main/java/com/ob/ejercicio3/Ejercicio3Application.java
@@ -1,13 +1,33 @@
 package com.ob.ejercicio3;
 
+import com.ob.ejercicio3.model.Employee;
+import com.ob.ejercicio3.repository.EmployeeRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Ejercicio3Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ejercicio3Application.class, args);
-	}
+    public static void main(String[] args) {
 
+        ApplicationContext context = SpringApplication.run(Ejercicio3Application.class, args);
+
+        EmployeeRepository repository = context.getBean(EmployeeRepository.class);
+
+        Employee ona = new Employee(null, "Ona", "Garniu", "123456789A", "ona@ona.com", 612345678);
+        Employee kalatrava = new Employee(null, "Kalatrava", "Garniu", "987654321B", "kalatrava@kalatrava.com", 698765432);
+        Employee kai = new Employee(null, "Kai", "Garniu", "999999999C", "kai@kai.com", 666666666);
+        repository.save(ona);
+        repository.save(kalatrava);
+        repository.save(kai);
+
+        System.out.println(repository.findById(2L));
+        System.out.println("El número actual de empleados es: " + repository.count());
+
+        System.out.println("Listado de empleados: ");
+        for (Employee employee : repository.findAll()) {
+            System.out.println(employee);
+        }
+    }
 }
---
+++ OB_Spring/sesiones_1_2_3/ejercicio3/src/main/java/com/ob/ejercicio3/model/Employee.java
@@ -0,0 +1,91 @@
+package com.ob.ejercicio3.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    private String dni;
+    private String email;
+    private Integer phoneNumber;
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String name, String surname, String dni, String email, Integer phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.dni = dni;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", dni='" + dni + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
+}
