0e4786a162944f040121138e937212607c52cf6f
oliverGarniu <oliver.garniu@gmail.com>
2022-11-28 19:58:21 +0100

    ejercicio 3, sesiones 10, 11 y 12

--- OB_Spring/sesiones_10_11_12/ejercicio3/src/main/java/com/ob/ejercicio3/config/WebSecurityConfig.java
+++ OB_Spring/sesiones_10_11_12/ejercicio3/src/main/java/com/ob/ejercicio3/config/WebSecurityConfig.java
@@ -1,10 +1,16 @@
 //package com.ob.ejercicio3.config;
 //
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
 //
 //@Configuration
 //@EnableWebSecurity
@@ -15,6 +21,7 @@
 //
 //        http.authorizeRequests()
 //                .antMatchers("/hello").permitAll()
+//                .antMatchers("/data").hasRole("ADMIN")
 //                .anyRequest().authenticated()
 //                .and()
 //                .formLogin()
@@ -24,6 +31,64 @@
 //
 //    @Override
 //    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        super.configure(auth);
+//        auth.inMemoryAuthentication()
+//                .passwordEncoder(new BCryptPasswordEncoder())
+//                .withUser("user").password(passwordEncoder().encode("password")).roles("USER")
+//                .and()
+//                .withUser("admin").password(passwordEncoder().encode("password")).roles("USER", "ADMIN");
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    @Override
+//    public UserDetailsService userDetailsServiceBean() throws Exception {
+//        return super.userDetailsServiceBean();
+//    }
+//
+//    @EnableWebSecurity
+//    @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+//    public class WebSecurityConfig {
+//        @Bean
+//        public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+//            InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//            manager.createUser(User.withUsername("user")
+//                    .password(bCryptPasswordEncoder.encode("userPass"))
+//                    .roles("USER")
+//                    .build());
+//            manager.createUser(User.withUsername("admin")
+//                    .password(bCryptPasswordEncoder.encode("adminPass"))
+//                    .roles("USER", "ADMIN")
+//                    .build());
+//            return manager;
+//        }
+//
+//        @Bean
+//        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//            http.csrf()
+//                    .disable()
+//                    .authorizeRequests()
+//                    .antMatchers(HttpMethod.DELETE)
+//                    .hasRole("ADMIN")
+//                    .antMatchers("/admin/**")
+//                    .hasAnyRole("ADMIN")
+//                    .antMatchers("/user/**")
+//                    .hasAnyRole("USER", "ADMIN")
+//                    .antMatchers("/login/**")
+//                    .anonymous()
+//                    .anyRequest()
+//                    .authenticated()
+//                    .and()
+//                    .httpBasic()
+//                    .and()
+//                    .sessionManagement()
+//                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//
+//            return http.build();
+//        }
 //    }
 //}
+//
